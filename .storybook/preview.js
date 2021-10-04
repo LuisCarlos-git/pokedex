0f4061a4f17f29fafafbd2310f2c0036d84c11b1
LuisCarlos-git <luisestrela.bol951@gmail.com>
2021-10-04 08:57:26 -0300

    style: add background default in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,19 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../src/styles/global';
 import theme from '../src/styles/theme';
 
+export const parameters = {
+  backgrounds: {
+    default: 'custom-dark',
+    values: [
+      {
+        name: 'custom-dark',
+        value: '#323240'
+      }
+    ]
+  }
+
+};
+
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
